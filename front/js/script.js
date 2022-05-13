7173a508baf6506b9806050e83fc5653cd6378ea
Theo <theo.larrue@yahoo.fr>
2022-05-13 15:46:41 +0200

    ajout du js sur index

---
+++ front/js/script.js
@@ -0,0 +1,57 @@
+// Card Builder
+
+var cardBuilder = function(product) {
+
+    //Creation de la balise <img> et ses attributs
+    const img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+
+    //Creation du titre <h3>
+    const h3 = document.createElement('h3');
+    h3.innerHTML = product.name;
+    h3.className = "productName";
+
+    //Creation du paragraphe de description
+    const p = document.createElement('p');
+    p.className = "productDescription";
+    p.innerHTML = product.description;
+
+    // Creation de la balise <article>
+    const article = document.createElement('article');
+    article.append(img, h3, p);
+
+
+    // Creation de la balise <a>
+    const link = document.createElement('a');
+    link.append(article);
+    link.href = '/product.html?id=' + product._id;
+
+
+    // Injection du contenu dans la <div> .items
+    const items = document.querySelector('.items');
+    items.append(link);
+
+}
+
+// GetApi
+
+var getApi = function() {
+    url = "http://localhost:3000/api/products";
+    fetch(url).then(res => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            console.log('error');
+        }
+    }).then(data => {
+        var products = data;
+        console.log(products)
+        for (let i = 0; i < products.length; i++) {
+            let product = products[i];
+            cardBuilder(product);
+        }
+    })
+}
+
+getApi();
